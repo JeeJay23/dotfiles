9ff5b6262ab065d6e4a8630213a54d3d6c06278c
Jader <jvjader@gmail.com>
2019-10-20 18:03:46 +0200

    added aliases to config

--- config.fish
+++ config.fish
@@ -1,5 +1,20 @@
 alias ls="ls -hN --color=auto --group-directories-first"
 
+abbr -a aptL 'apt list --installed | less'
+abbr -a e exit
+abbr -a g git
+abbr -a gc 'cd ~/repos/dotfiles'
+abbr -a gco 'git checkout'
+abbr -a gf 'git fetch'
+abbr -a gp 'git push'
+abbr -a mkd 'mkdir -pv'
+abbr -a r ranger
+abbr -a s 'sudo apt-get'
+abbr -a sn 'shutdown now'
+abbr -a v vim
+abbr -a ws '/home/silver/repos/pipes.sh/bin/bin/pipes.sh -p 3 -t 1 -r 3000 -R'
+abbr -a xc 'xclip -sel clip'
+
 function fish_greeting
     date +%B\ %d\ -\ %A
 end
